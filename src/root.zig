19624bfc4f2003df7838ad5f768233b8ce13c136
dean <deanvelopermn@gmail.com>
2024-07-30 09:06:30 -0700

    publish certain readers/writers

--- src/root.zig
+++ src/root.zig
@@ -5,6 +5,9 @@ pub const client = @import("./client.zig");
 pub const message = @import("./message.zig");
 
 pub const Connection = client.Connection;
+pub const Client = client.Client;
+pub const MessageReader = message.AnyMessageReader;
+pub const MessageWriter = message.AnyMessageWriter;
 
 test {
     std.testing.refAllDeclsRecursive(@This());
